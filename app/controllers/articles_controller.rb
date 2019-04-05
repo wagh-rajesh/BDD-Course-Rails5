50931d6ad07d88415b23082fac0f79f6a2524cde
Rajesh Wagh <rwaghgit@gmail.com>
2019-04-06 00:30:43 +0530

    Implement listing articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
-  def index; end
+  def index
+    @articles = Article.all
+  end
 
   def new
     @article = Article.new
---
+++ spec/features/listing_article_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.feature 'Listing Articles' do
+  before do
+    @article1 = Article.create(title: 'First Article', body: 'Lorem Ipsum First')
+    @article2 = Article.create(title: 'Second Article', body: 'Lorem Ipsum Second')
+  end
+
+  scenario 'A user lists all articles' do
+    visit '/'
+    expect(page).to have_content(@article1.title)
+    expect(page).to have_content(@article1.body)
+    expect(page).to have_content(@article2.title)
+    expect(page).to have_content(@article2.body)
+    expect(page).to have_link(@article1.title)
+    expect(page).to have_link(@article2.title)
+  end
+end
